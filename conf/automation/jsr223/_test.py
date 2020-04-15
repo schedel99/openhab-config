f171353fc8229d97b292895afb88991a23605b12
hhees <holger.hees@gmail.com>
2020-04-15 15:09:17 +0200

    initial commit

---
+++ conf/automation/jsr223/_test.py
@@ -0,0 +1,10 @@
+from custom.helper import rule
+from core.triggers import CronTrigger
+
+@rule("_test.py")
+class TestRule:
+    def __init__(self):
+        self.triggers = [CronTrigger("0 */30 * * * ?")]
+
+    def execute(self, module, input):
+        self.log.info("test rule triggered")
---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo Launching the openHAB runtime...
+
+DIRNAME=`dirname "$0"`
+
+#export JAVA_HOME="/dataDisk/java/"
+export JAVA_OPTS="${JAVA_OPTS} -Xbootclasspath/a:/opt/jython/dist/jython.jar -Dpython.home=/opt/jython/dist -Dpython.path=/dataDisk/openhab-runtime/python"
+#export JYTHONPATH="${DIRNAME}/python"
+export OPENHAB_HOME="${DIRNAME}"
+export OPENHAB_CONF="${DIRNAME}/conf"
+export OPENHAB_RUNTIME="${DIRNAME}/runtime"
+export OPENHAB_USERDATA="${DIRNAME}/userdata"
+export OPENHAB_LOGDIR="/dataDisk/var/log/openhab"
+
+exec "${DIRNAME}/runtime/bin/karaf" "${@}"
